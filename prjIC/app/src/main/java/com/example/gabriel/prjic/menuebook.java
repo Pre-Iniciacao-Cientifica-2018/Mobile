ebe280ffd7ae9c3ffdfa4a759130ff4d0f15095b
Gameleiro <30965065+JvStudent@users.noreply.github.com>
2019-03-22 16:56:12 -0300

    Trab do dia
    trabalheira toda pra isso, bixo

--- prjIC/app/src/main/java/com/example/gabriel/prjic/menuebook.java
+++ prjIC/app/src/main/java/com/example/gabriel/prjic/menuebook.java
@@ -2,47 +2,41 @@ package com.example.gabriel.prjic;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class menuebook extends AppCompatActivity {
 Intent intent;
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                  intent = new Intent(getApplicationContext(), MainActivity.class);
-                    return true;
-            }
-            return false;
-        }
-    };
-
+        ImageButton btnArrasta, btnGraph, btnHome;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menuebook);
+        setContentView(R.layout.menuebook);
+        btnHome = findViewById(R.id.btnHome);
+        btnArrasta= findViewById(R.id.btnArrasta);
+        btnGraph = findViewById(R.id.btnGraph);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentBtn(0);
+            }
+        });
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
 
 
     }
+    public void intentBtn(int i){
+
+
+        intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
+
 
 }
