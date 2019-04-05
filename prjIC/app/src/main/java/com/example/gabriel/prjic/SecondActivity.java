3fce9d44275641d97bb8ead47e56ce094eeb7fe8
ederzed <42612795+ederzed@users.noreply.github.com>
2019-04-05 15:13:35 -0300

    Atualizações de design e navegação
    ...

---
+++ prjIC/app/src/main/java/com/example/gabriel/prjic/SecondActivity.java
@@ -0,0 +1,179 @@
+package com.example.gabriel.prjic;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    Intent intent;
+    ImageButton btnArrasta, btnGraph, btnHome;
+    private LinearLayout lay = null;
+    private ImageView img = null;
+    private ImageView img1 = null;
+    DrawerLayout drawer;
+    TextView txtTitulo;
+    //private float x,y;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        btnHome = findViewById(R.id.btnHome);
+        btnArrasta = findViewById(R.id.btnArrasta);
+        btnGraph = findViewById(R.id.btnGraph);
+        txtTitulo = findViewById(R.id.txtTitulo);
+        drawer  = (DrawerLayout) findViewById(R.id.drawer_layout);
+        btnArrasta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Btn(1);
+            }
+        });
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Btn(0);
+            }
+        });
+        btnGraph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Btn(2);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        lay = (LinearLayout) findViewById(R.id.linear);
+        img = (ImageView) findViewById(R.id.imageView5);
+        img1 = (ImageView) findViewById(R.id.imageView9);
+        lay.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        //    x = event.getX();
+                        //    y = event.getY(); Um dia pode ser util
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            if (img.getVisibility() == View.VISIBLE && img1.getVisibility() == View.VISIBLE) {
+
+                                img.setVisibility(View.INVISIBLE);
+                                img1.setVisibility(View.INVISIBLE);
+                            } else {
+                                img.setVisibility(View.VISIBLE);
+                                img1.setVisibility(View.VISIBLE);
+                            }
+
+                        }
+
+                return true;
+            }
+
+        });
+    }
+
+    public void Btn(int i) {
+
+        switch (i) {
+            case 0:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case 1:
+                drawer.openDrawer(GravityCompat.START);
+            break;
+            case 2:
+                intent = new Intent(this, MedicaoReal.class);
+                startActivity(intent);
+                break;
+
+        }
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Intent intent;
+        if (id == R.id.itemCap1) {
+            txtTitulo.setText(R.string.cap1);
+        } else if (id == R.id.itemCap2) {
+
+        } else if (id == R.id.itemCap3) {
+
+        } else if (id == R.id.itemCap4) {
+
+        } else if (id == R.id.itemInicio) {
+
+            intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.itemMedReal) {
+            intent = new Intent(this, MedicaoReal.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
